a59a70e00a1c604d3fecf21aa5921fc8a2078d50
Brianna Foley <brianna.foley87@gmail.com>
2022-03-07 17:37:27 -0600

    migrations for main models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :gadget
-  belongs_to :customer
+  belongs_to :customer, class_name: "User"
 end
--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,3 +1,4 @@
 class Gadget < ApplicationRecord
-  belongs_to :owner
+  belongs_to :owner, class_name: "User"
+  has_many :bookings
 end
