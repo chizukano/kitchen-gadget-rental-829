7d7c17c24bf2aeb9607dd1d9ba52ae910e7f518d
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2022-03-08 14:06:17 -0500

    Revert "Created a show feature for gadgets"

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -2,8 +2,4 @@ class GadgetsController < ApplicationController
   def index
     @gadgets = Gadget.all
   end
-
-  def show
-    @gadget = Gadget.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :gadgets, only: %i[index show]
+  resources :gadgets, only: [:index]
   devise_for :users
   root to: 'pages#home'
 end
