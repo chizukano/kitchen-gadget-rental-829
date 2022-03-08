2ed3edc86c702ff760557e2796d4c4a5189168a2
David Nicolson <ddn100@yahoo.co.uk>
2022-03-08 16:23:10 -0600

    Created show method in booking controler, created show.html.erb page to display rented items

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,9 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+ def show
+  @booking = Booking.find(params[:id])
+  @gadget = Gadget.find(params[:id])
+ end
+
 end
