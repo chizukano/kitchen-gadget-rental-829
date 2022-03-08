916d0195cc00c0ac2d13342e2999ae4ce69b06df
chizukano <chizu0303@gmail.com>
2022-03-08 13:05:03 -0600

    added new page for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :gadgets, only: [:index]
+  resources :gadgets, only: [:index] do
+    resources :bookings, only: [:new, :create]
+  end
   devise_for :users
   root to: 'pages#home'
 end
