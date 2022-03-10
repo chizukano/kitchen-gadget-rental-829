df49f0d563f42d277f58169e434db865869b5d92
Brianna Foley <brianna.foley87@gmail.com>
2022-03-10 16:42:44 -0600

    seeded photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,21 @@ customers = [customer_1, customer_2]
 puts "done!"
 
 puts "creating 15 fake gadgets"
-gadgets_name = %w[knives tongs frying-pan saucepan stockpot can-opener salad-spinner]
+gadgets_name = ["Knives", "Tongs", "Frying Pan", "Dutch Oven", "Stockpot",
+                "Citrus Juicer", "Espresso Machine", "Food Processor",
+                "French Press", "Mandoline Slicer", "Percolator", "Pizza Slicer",
+                "Stand Mixer", "Toaster Oven", "Whisk", "Wok"]
 gadgets = 15.times.map do
-  gadget = Gadget.create!(
-    name: gadgets_name.sample,
+  gadget_name = gadgets_name.sample
+  file = File.open("db/support/#{gadget_name.parameterize}.jpg")
+  gadget = Gadget.new(
+    name: gadget_name,
     description: Faker::Lorem.sentence(word_count: rand(5..20)),
     owner: owner
   )
+  gadget.photo.attach(io: file, filename: "#{gadget_name.parameterize}.jpg", content_type: 'image/jpg')
+  gadget.save!
+
   gadget.update_columns(
     latitude: rand(19.4108923..19.416905),
     longitude: rand(-99.1720769..-99.1606911)
