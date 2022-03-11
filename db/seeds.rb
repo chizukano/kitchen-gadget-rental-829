c2985327f1306d7595d100900f745ba23858ba51
chizukano <chizu0303@gmail.com>
2022-03-11 11:58:56 -0600

    Change done

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,8 +38,8 @@ puts "done!"
 puts "creating 10 fake bookings"
 10.times do
   booking = Booking.create!(
-    start_on: Faker::Date.between(from: '2021-03-01', to: '2021-03-31'),
-    end_on: Faker::Date.between(from: '2021-04-01', to: '2021-04-30'),
+    start_on: Faker::Date.between(from: '2022-03-01', to: '2022-03-31'),
+    end_on: Faker::Date.between(from: '2022-03-05', to: '2022-04-30'),
     gadget: gadgets.sample,
     customer: customers.sample,
   )
