9b97dde49e303a15574f89e1a988c0c67f866ffb
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-01-15 16:52:20 -0600

    fly.io deploy files

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+
+  # namespace :user do
+  #   resource :chef, only: %i[new create]
+  # end
+  # namespace :chef do
+  #   resource :dashboard, only: :show
+  # end
+  # resources :chefs, only: %i[show]
+
+  # resource :hello
 end
